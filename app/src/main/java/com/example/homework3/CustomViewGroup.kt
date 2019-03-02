19600543a40956105966407ad62f5d195e710a81
NorraBjornen <alexmur20000@mail.ru>
2019-03-02 13:42:42 +0500

    Initial commit

---
+++ app/src/main/java/com/example/homework3/CustomViewGroup.kt
@@ -0,0 +1,64 @@
+package com.example.homework3
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.ViewGroup
+
+class CustomViewGroup @JvmOverloads constructor(context: Context,attrs: AttributeSet? = null,defStyleAttr: Int = 0) : ViewGroup(context, attrs, defStyleAttr) {
+
+    private val distanceBetweenViews : Int
+    private val verticalPadding = 20
+    private val childViewHeight = 50
+
+    init {
+        val typedArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomViewGroup)
+        distanceBetweenViews = typedArray.getInt(R.styleable.CustomViewGroup_cvg_distance_between_views, 0)
+        typedArray.recycle()
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val width = MeasureSpec.getSize(widthMeasureSpec)
+
+        val childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(width, MeasureSpec.UNSPECIFIED)
+        val childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(childViewHeight, MeasureSpec.EXACTLY)
+
+        for (index in 0 until childCount)
+            getChildAt(index).measure(childWidthMeasureSpec, childHeightMeasureSpec)
+
+        var usedHeight = 0
+        var usedWidthInRow = 0
+
+        for (index in 0 until childCount) {
+            val child = getChildAt(index)
+            if(usedWidthInRow + distanceBetweenViews + child.measuredWidth < width)
+                usedWidthInRow += (distanceBetweenViews + child.measuredWidth)
+            else {
+                usedWidthInRow = distanceBetweenViews + child.measuredWidth
+                usedHeight += (childViewHeight + verticalPadding)
+            }
+        }
+
+        setMeasuredDimension(width, usedHeight + childViewHeight)
+    }
+
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        val width = r - l
+
+        var usedHeight = 0
+        var usedWidthInRow = 0
+
+        for (index in 0 until childCount) {
+            val child = getChildAt(index)
+            if(usedWidthInRow + distanceBetweenViews + child.measuredWidth < width){
+                child.layout(usedWidthInRow, usedHeight, usedWidthInRow + child.measuredWidth, usedHeight + child.measuredHeight)
+                usedWidthInRow += (distanceBetweenViews + child.measuredWidth)
+            }
+            else {
+                usedHeight += (child.measuredHeight + verticalPadding)
+                child.layout(0, usedHeight, child.measuredWidth,usedHeight + child.measuredHeight)
+                usedWidthInRow = distanceBetweenViews + child.measuredWidth
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/homework3/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.homework3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val cvg = findViewById<CustomViewGroup>(R.id.customViewGroup)
+
+        val data = arrayListOf(
+            "hello darkness",
+            "my old friend",
+            "i've come",
+            "to talk with you",
+            "again",
+            "because a vision softly creeping",
+            "left its seeds while I was sleeping",
+            "and the vision that was planted in my brain",
+            "still remains",
+            "within",
+            "the sound",
+            "of silence"
+        )
+
+        for(i in 0 until data.size){
+            val txt = TextView(this)
+            txt.text = data[i]
+            cvg.addView(txt)
+        }
+    }
+}
